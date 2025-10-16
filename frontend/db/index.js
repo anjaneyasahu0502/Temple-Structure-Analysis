305a32aa985852ed096a4ec72aca7d2ab988c4d6
anjaneyasahu0502 <136296617+anjaneyasahu0502@users.noreply.github.com>
2025-10-16 11:00:55 +0530

    Implement MongoDB connection in index.js

---
+++ frontend/db/index.js
@@ -0,0 +1,18 @@
+
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+
+const connectDB= async()=>{
+    try{
+
+     const connectionInstance=  await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+     console.log(`\n MOngoDB connected !! DB host:${connectionInstance.connection.host}`);
+    }catch(error){
+        console.log("MONGODB connection failed",error);
+        
+    }
+}
+
+export default connectDB;
